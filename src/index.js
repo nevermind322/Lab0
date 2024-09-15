44a6ea1da80f9cc06517e13ccbbab847b259096e
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2024-09-15 14:45:00 +0300

    var4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-alert("Let's integrate!")
+alert("Let's integrate!");
+p1 = prompt("a = ", 0);
+p2 = prompt("b = ", 0);
+function calculate(a, b, f) {
+    if (isNaN(a) || isNaN(b))
+        alert("Wrong input!")      
+    else {
+        a = Math.round(a)
+        b = Math.round(b)
+    }
+    if (a > b) {
+        alert("Wrong interval!")  
+    }
+    n = 1000
+    sum = f(a) + f(b)
+    dx = (b-a)/n
+    for (let i = 1; i < n; i++ ) {
+        sum += 2 * f(a + dx*i)
+    }
+    sum /=2
+    sum *=dx
+    alert("S(trap) = " + sum);
+}
+
+let var4  = (x) => x*x -x + 1 
+
+calculate(p1, p2, var4)
